5925e39dd02ad6377c46a489bfb68671f248d980
bingbinglee <supriyo789.next@gmail.com>
2020-04-08 19:28:29 -0400

    first commit to test

---
+++ js/global.js
@@ -0,0 +1,69 @@
+
+var imgURL = './images/idena-logo-dark.svg';
+var moonURL = './images/moon.svg';
+var sunURL = './images/sun.svg';
+var parent = document.querySelector(".header_logo");
+
+parent.querySelector("a img").src = imgURL;
+var mode = localStorage.getItem('mode') || '';
+
+
+
+var parent2 = document.querySelector(".justify-content-between");
+var modechange = parent2.querySelector(".col-auto:last-child");
+
+
+document.getElementsByClassName('header')[0].classList.add('fixed-header');
+document.getElementsByClassName('main')[0].classList.add('extra-main-padding');
+
+
+var scrolltop = '<button type="button" class="btn btn-icon hide" id="scrollTop" onclick="window.scrollTo({ top: 0, behavior: \'smooth\' });">'
+				+'<i class="icon icon--thin_arrow_up"></i></button>';
+
+document.querySelector(".section:last-child").innerHTML = document.querySelector(".section:last-child").innerHTML + scrolltop;
+
+
+var htmcontent = '<button type="button" id="moon" class="rem btn btn-icon"'
+						+' onclick="localStorage.setItem(\'mode\', \'dark\');'
+            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\''+imgURL+'\');'
+            +'document.getElementsByTagName(\'html\')[0].classList.add(\'darkmode\');'
+            +'document.getElementById(\'moon\').classList.add(\'rem\');'
+            +'document.getElementById(\'sun\').classList.remove(\'rem\');'
+						+'" id="moon">'
+						+'<img src="'+moonURL+'" width="18px" height="18px"' 
+						+'style="opacity: 0.8;"/></button>'
+            +'<button type="button" id="sun" class="rem btn btn-icon"'
+						+' onclick="localStorage.setItem(\'mode\', \'light\'); document.getElementsByTagName(\'html\')[0].classList.remove(\'darkmode\');'
+            +'document.querySelector(\'.header_logo a img\').setAttribute(\'src\',\'https://scan.idena.io/images/idena-logo.svg\');'
+            +'document.getElementById(\'moon\').classList.remove(\'rem\');'
+            +'document.getElementById(\'sun\').classList.add(\'rem\');'
+						+'" id="sun">'
+						+'<img src="'+sunURL+'" width="18px" height="18px"' 
+						+'style="opacity: 0.8;"/></button>'; 
+
+modechange.innerHTML = htmcontent;
+
+
+if (mode =='dark') {
+  parent.querySelector("a img").src = imgURL;
+  document.getElementById('sun').classList.remove('rem');
+
+} else {
+  parent.querySelector("a img").src = 'https://scan.idena.io/images/idena-logo.svg';
+  document.getElementById('moon').classList.remove('rem');
+} 
+
+
+var sticky = document.getElementsByClassName('header')[0].offsetTop;
+
+window.onscroll = function() {shadowFunction()};
+
+function shadowFunction() {
+  if (window.pageYOffset > sticky) {
+    document.getElementsByClassName('header')[0].classList.add('header-shadow');
+    document.getElementById('scrollTop').classList.remove('hide');
+  } else {
+    document.getElementsByClassName('header')[0].classList.remove('header-shadow');
+    document.getElementById('scrollTop').classList.add('hide');
+  }
+}
---
+++ js/preload.js
@@ -0,0 +1,14 @@
+
+var imgURL = './images/idena-logo-dark.svg';
+var moonURL = './images/moon.svg';
+var sunURL = './images/sun.svg';
+var mode = localStorage.getItem('mode') || '';
+
+
+if (mode =='dark') {
+  document.getElementsByTagName('html')[0].classList.add('darkmode');
+
+} else {
+  
+} 
+
