540f3c67c31d8a635b0b6b73247dad9210796d8b
bingbinglee <supriyo789.next@gmail.com>
2020-04-08 22:57:05 -0400

    added explorer tab

--- js/home-loader.js
+++ js/home-loader.js
@@ -20,9 +20,12 @@ function ajax_get(url, callback) {
 
 var videolist = document.getElementById("videos-list");
 var comlist = document.getElementById("communities-list");
+var exlist = document.getElementById("explorer-list");
 var comcontent = '';
 var videocontent = '';
+var excontent = '';
 var official = '';
+var git = '';
 
 
 window.onload = (function(){
@@ -95,6 +98,47 @@ window.onload = (function(){
       
   }); //fetching all the communities listed
 
+
+
+
+
+//load all explorers
+  ajax_get('http://idena-apps.org/sources/explorer.json', function(data) {
+    //console.log(data);
+
+    data["entries"].forEach(function(obj) { 
+
+    	if(obj.official=="no") {
+    		console.log(obj.official);
+    		official = '<span class="badge badge-secondary hide">Official</span>';
+    	} else {
+    		official = '<span class="badge badge-secondary">Official</span>';
+    	}
+    	if(obj.github!=""){
+    		git = '<a class="btn btn-secondary btn-small" href="'+obj.github+'" target="_blank">'
+                            +'<span>Github Project Page</span>'
+                            +'<i class="icon icon--thin_arrow_right"></i>'
+                          +'</a>';
+    	} else { git = ''; }
+    	excontent = excontent + '<div class="col-12 col-sm-3 entry">'
+                          +'<div class="mini-card">'
+                          +'<div class="thumbnail" style="background-image: url('+obj.image_url+');"></div>'
+                          +'<p class="desc">'
+                           +obj.short_description
+                            +official
+                          +'</p>'
+                          +'<a class="btn btn-secondary btn-small" href="'+obj.url+'" target="_blank">'
+                            +'<span>Visit Website</span>'
+                            +'<i class="icon icon--thin_arrow_right"></i>'
+                          +'</a>'
+                          +git
+                          +'</div>'
+                        +'</div>'; 
+    });
+
+    exlist.innerHTML = excontent;
+      
+  }); //fetching all the communities listed
 
 
 
