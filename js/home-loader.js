3bedfd370b19c3d1af6a63f6dc77187b88ee3310
bingbinglee <supriyo789.next@gmail.com>
2020-04-09 16:21:03 -0400

    Apps tab live
    opensource unavailable will show a disabled button instead

--- js/home-loader.js
+++ js/home-loader.js
@@ -21,15 +21,62 @@ function ajax_get(url, callback) {
 var videolist = document.getElementById("videos-list");
 var comlist = document.getElementById("communities-list");
 var exlist = document.getElementById("explorer-list");
+var appslist = document.getElementById("apps-list");
 var comcontent = '';
 var videocontent = '';
 var excontent = '';
+var appscontent = '';
 var official = '';
 var git = '';
 var caption = '';
 
 
 window.onload = (function(){
+
+
+  //load all apps
+  ajax_get('http://idena-apps.org/sources/apps.json', function(data) {
+    //console.log(data);
+
+    data["entries"].forEach(function(obj) { 
+
+      if(obj.official=="no") {
+        //console.log(obj.official);
+        official = '<span class="badge badge-secondary hide">Official</span>';
+      } else {
+        official = '<span class="badge badge-secondary">Official</span>';
+      }
+      if(obj.github!=""){
+        git = '<a class="btn btn-secondary btn-small" href="'+obj.github+'" target="_blank">'
+                            +'<span>Github Project Page</span>'
+                            +'<i class="icon icon--thin_arrow_right"></i>'
+                          +'</a>';
+      } else { git = '<a class="btn btn-secondary btn-small" href="'+obj.github+'" disabled>'
+                            +'<span>Github Unavailable</span>'
+                            +'<i class="icon icon--thin_arrow_right"></i>'
+                          +'</a>'; }
+      appscontent = appscontent + '<div class="col-12 col-sm-3 entry">'
+                          +'<div class="mini-card">'
+                          +'<div class="thumbnail" style="background-image: url('+obj.image_url+');">'+official+'</div>'
+                          +'<p class="desc" style="padding-bottom: 0px;">'
+                           +obj.short_description
+                          +'</p>'
+                          +'<p class="control">By '+obj.created_by+'</p>'
+                          +'<a class="btn btn-secondary btn-small" href="'+obj.url+'" target="_blank">'
+                            +'<span>See App</span>'
+                            +'<i class="icon icon--thin_arrow_right"></i>'
+                          +'</a>'
+                          +git
+                          +'</div>'
+                        +'</div>'; 
+    });
+
+    appslist.innerHTML = appscontent;
+      
+  }); //fetching all the apps listed
+
+
+
 
   //load all videos
   ajax_get('http://idena-apps.org/sources/videos.json', function(data) {
@@ -54,7 +101,7 @@ window.onload = (function(){
                           +'<p class="desc" style="padding-bottom: 0px;">'
                            +obj.short_description                          
                           +'</p>'
-                          +'<p class="control">Made By '+obj.created_by+'</p>'
+                          +'<p class="control">By '+obj.created_by+'</p>'
                           +'<a class="btn btn-secondary btn-small" href="'+obj.url+'" target="_blank">'
                             +'<span>'+caption+'</span>'
                             +'<i class="icon icon--thin_arrow_right"></i>'
@@ -132,7 +179,7 @@ window.onload = (function(){
                           +'<p class="desc" style="padding-bottom: 0px;">'
                            +obj.short_description
                           +'</p>'
-                          +'<p class="control"> Made By'+obj.created_by+'</p>'
+                          +'<p class="control">By '+obj.created_by+'</p>'
                           +'<a class="btn btn-secondary btn-small" href="'+obj.url+'" target="_blank">'
                             +'<span>Visit Website</span>'
                             +'<i class="icon icon--thin_arrow_right"></i>'
