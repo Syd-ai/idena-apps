4c897ad154cdafa6eeaee184fba8ddfe65b8d59e
bingbinglee <supriyo789.next@gmail.com>
2020-04-08 20:33:27 -0400

    commit videos.json file

---
+++ js/home-loader.js
@@ -0,0 +1,32 @@
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+
+window.onload = (function(){
+
+  ajax_get('./sources/videos.json', function(data) {
+    //console.log(data);
+
+    console.log(data['entries'][0]);
+      
+  });
+
+
+})();
