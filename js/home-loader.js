d6758ddf58904a3288ef86632efe33e6c5a10b2b
bingbinglee <supriyo789.next@gmail.com>
2020-04-15 23:29:55 -0400

    Added validation time and identities

--- js/home-loader.js
+++ js/home-loader.js
@@ -18,50 +18,126 @@ function ajax_get(url, callback) {
 }
 
 
+function timemagic(){
+  var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("counter").innerHTML = '<div class="col-auto">'
+                    +'<span class="days">'+days+'</span>'
+                    +'<span class="_smalltext"> days</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="hours">'+hours+'</span>'
+                    +'<span class="_smalltext"> hours</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="minutes">'+minutes+'</span>'
+                    +'<span class="_smalltext"> min</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="seconds">'+seconds+'</span>'
+                    +'<span class="_smalltext"> seconds</span>'
+                  +'</div>';
+  document.getElementById("NextValidationDateTime").innerHTML = validTime;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}
+
+
+
+var onlineid = '<div class="col-12 col-sm-3">'
+            +'<h1>Identities</h1>'
+            +'<div class="card">'
+            +'<div class="info_block">'
+             +'<div class="row">'
+              +'<div class="col-12 col-sm-12 bordered-col">'
+                    +'<h3 class="info_block__accent"><span id="OnlineMinersTotal">--</span> / <span id="ValidatedTotal">--</span></h3>'
+                    +'<div class="control-label" data-toggle="tooltip" title="" data-original-title="Online miners / Total validated identities">Online / Total</div>'
+                  +'</div>'
+              +'</div>'
+              +'</div>'
+              +'</div></div>';
+
+  var timewidget = '<div class="row">'
+            +onlineid
+            +'<div class="col-12 col-sm-9">'
+            +'<h1>Next Validation In</h1>'
+            +'<div class="card">'
+            +'<div class="info_block">'
+            +'<div class="row">'
+              +'<div class="col-12 col-sm-12 bordered-col lead_info__counter" id="TimerPanel">'
+                +'<div id="counter" class="_value row justify-content-center">'
+                  +'<div class="col-auto">'
+                    +'<span class="days">--</span>'
+                    +'<span class="_smalltext"> days</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="hours">--</span>'
+                    +'<span class="_smalltext"> hours</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="minutes">--</span>'
+                    +'<span class="_smalltext"> min</span>'
+                  +'</div>'
+                  +'<div class="col-auto">'
+                    +'<span class="seconds">--</span>'
+                    +'<span class="_smalltext"> seconds</span>'
+                  +'</div>'
+                +'</div>'
+                +'<div class="control-label" data-toggle="tooltip">'
+                  +'Validation time: <span id="NextValidationDateTime">--</span>'
+                +'</div>'
+              +'</div>'
+              +'</div>'
+              +'</div>'
+              +'</div>'              
+              +'</div>'
+              
+            +'</div>';
+
+
 
 window.onload = (function(){
 
-/*
+  document.getElementById("time_and_identity").innerHTML = timewidget;
 
-//load all explorers
-  ajax_get('https://idena-apps.org/sources/explorer.json', function(data) {
-    //console.log(data);
+  ajax_get('https://api.idena.io/api/epoch/last', function(data) {
+      countDownDate = new Date(data['result']['validationTime']).getTime();
+      var d = new Date(data['result']['validationTime']);
+      validTime = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
+      timemagic();
+      });
 
-    data["entries"].forEach(function(obj) { 
+  ajax_get('https://api.idena.org/api/OnlineIdentities/Count', function(data) {
+      var countnum = data['result'];
+      document.getElementById("ValidatedTotal").innerHTML = countnum;
+      });
 
-    	if(obj.official=="no") {
-    		//console.log(obj.official);
-    		official = '<span class="badge badge-secondary hide">Official</span>';
-    	} else {
-    		official = '<span class="badge badge-secondary">Official</span>';
-    	}
-    	if(obj.github!=""){
-    		git = '<a class="btn btn-secondary btn-small" href="'+obj.github+'" target="_blank">'
-                            +'<span>Github Project Page</span>'
-                            +'<i class="icon icon--thin_arrow_right"></i>'
-                          +'</a>';
-    	} else { git = ''; }
-    	excontent = excontent + '<div class="col-12 col-sm-3 entry">'
-                          +'<div class="mini-card">'
-                          +'<a href="'+obj.url+'" target="_blank"><div class="thumbnail" style="background-image: url('+obj.image_url+');">'+official+'</div></a>'
-                          +'<p class="desc" style="padding-bottom: 0px;">'
-                           +obj.short_description
-                          +'</p>'
-                          +'<p class="control">By '+obj.created_by+'</p>'
-                          +'<a class="btn btn-secondary btn-small" href="'+obj.url+'" target="_blank">'
-                            +'<span>Visit Website</span>'
-                            +'<i class="icon icon--thin_arrow_right"></i>'
-                          +'</a>'
-                          +git
-                          +'</div>'
-                        +'</div>'; 
-    });
 
-    exlist.innerHTML = excontent;
-      
-  }); //fetching all the communities listed
+  ajax_get('https://api.idena.org/api/OnlineMiners/Count', function(data) {
+      var countnum = data['result'];
+      document.getElementById("OnlineMinersTotal").innerHTML = countnum;
+      });
 
-*/
+
+
 
 
 })();
