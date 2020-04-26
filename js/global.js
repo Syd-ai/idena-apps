8a310e02a1d8efe2064c43b1fccbbdfc9753b0be
bingbinglee <supriyo789.next@gmail.com>
2020-04-26 10:17:23 -0400

    French translation by @syd

--- js/global.js
+++ js/global.js
@@ -47,6 +47,7 @@ modechange.innerHTML = htmcontent;
 
 var langcontent = '<select id="langSelect" onchange="langchange()">'
 +'<option id="lang_en" value="en">English - EN</option>'
++'<option id="lang_fr" value="en">French - FR</option>'
 +'<option id="lang_hi" value="hi">Hindi - HI</option>'
 +'<option id="lang_id" value="id">Indonesian - ID</option>'
 +'</select>';
