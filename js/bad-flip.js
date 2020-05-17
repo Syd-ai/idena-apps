39700f6e1d5f2847ef584ffea463543fdf06c0d0
bingbinglee <supriyo789.next@gmail.com>
2020-05-16 22:37:04 -0400

    add local translation js to flip pages

---
+++ js/bad-flip.js
@@ -0,0 +1,60 @@
+var lang = localStorage.getItem('lang') || 'en';
+
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+var comlist = document.getElementById("communities-list");
+var comcontent = '';
+var official = '';
+var git = '';
+var caption = '';
+
+
+window.onload = (function(){
+    
+
+    ajax_get('https://idena-apps.org/locale/'+lang+'.json', function(data2) {
+
+                //load all page lang
+                document.getElementById("back").innerHTML = data2["back"];
+                document.getElementById("title").innerHTML = data2["bad_flip_t"];
+                document.getElementById("page-title").innerHTML = data2["bad_flip_title"];
+                document.getElementById("page_subtext").innerHTML = data2["bad_flip_subtext"];
+
+                document.getElementById("bad_flip_1").innerHTML = data2["bad_flip_1"];
+                document.getElementById("bad_flip_2").innerHTML = data2["bad_flip_2"];
+                document.getElementById("bad_flip_3").innerHTML = data2["bad_flip_3"];
+                document.getElementById("bad_flip_4").innerHTML = data2["bad_flip_4"];
+                document.getElementById("bad_flip_5").innerHTML = data2["bad_flip_5"];
+                document.getElementById("bad_flip_6").innerHTML = data2["bad_flip_6"];
+                document.getElementById("bad_flip_7").innerHTML = data2["bad_flip_7"];
+                document.getElementById("bad_flip_8").innerHTML = data2["bad_flip_8"];
+
+                document.getElementById("disclaimer").innerHTML = data2["disclaimer"];
+                document.getElementById("donation").innerHTML = '<p class="desc" style="line-height: 2em;">'+data2["donate_pretext"] 
+                +'<a href="http://github.com/bingbinglee/" target="_blank">@bingbinglee.</a>'+data2["donate_posttext"]+'<a href="https://scan.idena.io/address?address=0x140d5add76f3e4cc4538b9809601383bd74689df" target="_blank">'
+                +'<span class="donate">0x140d5add76f3e4cc4538b9809601383bd74689df</span></a></p>';
+
+                //page lang load ends
+
+    });
+
+
+})();
